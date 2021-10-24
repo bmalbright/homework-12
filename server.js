6e6e8dc00226eab95bd5c5f4e2642d55b667dde3
bmalbright <brianalbright@utexas.edu>
2021-10-24 18:47:48 -0500

    added to server.js. Set up initial prompt question. Wrote some initial responses.

--- server.js
+++ server.js
@@ -14,8 +14,49 @@ const db = mysql2.createConnection( {
   console.log(`Connected to the whac_employees_db database.`)
 );
 
+//checking connection
 db.query('SELECT * FROM employees', function (err, results) {
   console.log(results);
   console.table(results);
 });
 
+// Converting HW 10 for this
+
+function init() {
+  inquirer.prompt(initialPrompt).then((data) => {
+ 
+    if(data.initialOptions==='View all departments') {
+      console.table(departments);
+    }
+    if(data.initialOptions==='View all roles') {
+      console.table(roles);
+    }
+ //    console.log(teamMembers);
+    if(data.initialOptions==='View all employees') {
+     // const employees = new Intern(data.name, data.id, data.email, data.college)
+      console.table(employees);
+    }
+
+    Const initialPrompt = [
+      {
+        type: "list",
+        name: "initialOptions",
+        message: "Please choose an option.",
+        choices: [
+          'View all departments', 
+          'View all roles', 
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee'
+        ],
+      },
+    ];
+
+
+
+
+
+
+init ();
